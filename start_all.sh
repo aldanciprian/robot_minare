7c45502b83ff19e205bf14287d5923134a4bf8dd
aldanciprian <aldanciprian@gmail.com>
2017-06-20 15:27:36 +0300

    remote control

--- start_all.sh
+++ start_all.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
 
+
+BASE=/media/sf_shared/temp/nicehash_mining/robot_minare/
+res=0
+startup=0
 while [ 1 ]
 do
-	echo "started all" > /tmp/$$_$0.log
-	ps -ef
-	sleep 1s
+	echo "========================"
+	date
+	cd $BASE/
+	git pull
+	Startup=$(cat control.txt | grep startup | awk -F"=" '{print $2}')
+	echo ${Startup}
+	if [  ${Startup} = "on" ]
+	then
+		res=$(ps -ef | grep  simpleNiceHash.pl | grep -v grep )
+		if [ $? -ne 0 ]
+		then
+			echo "Starting simpleNiceHash.pl"
+			./simpleNiceHash.pl &
+			res=$(ps -ef | grep  simpleNiceHash.pl | grep -v grep | awk '{print $2}' )
+		else
+			res=$(echo $res | awk '{print $2}')
+			echo "simpleNiceHash.pl allready started $res"
+		fi
+	else
+		echo "Off"
+		if [ $res -ne 0 ]
+		then 
+			echo "Killing $res"
+			kill -9 $res
+			res=0
+		fi
+	fi
+
+	sleep 2s
 done
